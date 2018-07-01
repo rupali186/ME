3554609ecb48243e6e1f10eb1a62627c436cfaad
rupali186 <35391407+rupali186@users.noreply.github.com>
2018-07-01 20:22:01 +0530

    first commit

---
+++ app/src/main/java/com/example/rupali/mahimaelectronics/SignIn.java
@@ -0,0 +1,21 @@
+package com.example.rupali.mahimaelectronics;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class SignIn extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.sign_in, container, false);
+        return rootView;
+    }
+}
